d3fec89738391830773d0160a9be28427205af66
Ohashi-Dev <ohashi.developer@gmail.com>
2022-09-18 22:10:53 +0900

    fix

--- app/test.c
+++ app/test.c
@@ -124,7 +124,7 @@ void main()
   }
 
   ////////////////////// Set Matrix /////////////////////////////
-  int matrix[4][64];
+  int matrix[8][128];
 
   // matrixはまとめて送られる設定にしていない、まとめて送ってしまっても、使えずに終わるので
   // まとめて送る分は同じデータにしておいて対応をしているっぽい
@@ -132,15 +132,14 @@ void main()
   // coreで渡しているデータを見たらわかる
   // mat_dに渡しているのはS_AXIS_TDATA[31:0]だけ
   printf("\n--- Set Matrix ---\n");
-  for (int j = 0; j < 4; j++)
+  for (int j = 0; j < 8; j++)
   {
-    for (int i = 0; i < 64; i++)
+    for (int i = 0; i < 128; i++)
     {
       matrix[j][i] = rand() & 0x000000ff;
       printf("%3d ", matrix[j][i]);
       // src[(j * 8 + i)] = matrix[j][i]; // これやと失敗する
-      src[(j * 8 + i) * 2 + 0] = matrix[j][i]; // 0 2 4 6 8 10 12 14 | 16 18 20 22 24 26 28 30 | 32 ... 62
-      src[(j * 8 + i) * 2 + 1] = matrix[j][i]; // 1 3 5 7 9 11 13 15 | ...                              63
+      src[j * 128 + i] = matrix[j][i]; // 0 2 4 6 8 10 12 14 | 16 18 20 22 24 26 28 30 | 32 ... 62
     }
     printf("\n");
   }
@@ -153,9 +152,9 @@ void main()
   // AXI DMA transfer tx
   // 入力データをUIOで設定 (入力を設定する場所は一定)
   dma_reset();
-  dma[0x00 / 4] = 1;             // 転送開始
-  dma[0x18 / 4] = src_phys;      // 格納先のメモリアドレス
-  dma[0x28 / 4] = 8 * 4 * 4 * 2; // ワード単位の長さ単位 // 64 * 4 * 4 * 2 をしないといけないね。上のやつを見ると。 // 64 * 4個を4バイト分確保。そして二倍にして送る
+  dma[0x00 / 4] = 1;           // 転送開始
+  dma[0x18 / 4] = src_phys;    // 格納先のメモリアドレス
+  dma[0x28 / 4] = 8 * 128 * 4; // ワード単位の長さ単位 // 64 * 4 * 4 * 2 をしないといけないね。上のやつを見ると。 // 64 * 4個を4バイト分確保。そして二倍にして送る
   // dma[0x28 / 4] = 8 * 4 * 4;
 
   // Wait for the tx to finish
@@ -171,7 +170,7 @@ void main()
   // run <- 1;
   top[0x00 / 4] = 2;
 
-  int sample[4][8];
+  int sample[8][128];
 
   // tb.cppでは16個でまとめて64送っている
   // ここではまとめてsrcの01 23 45 が送られる？
@@ -185,7 +184,7 @@ void main()
     {
       sample[j][i] = rand() & 0x000000ff;
       printf("%3d ", sample[j][i]);
-      src[j * 8 + i] = sample[j][i];
+      src[j * 128 + i] = sample[j][i];
     }
     printf("\n");
   }
@@ -200,7 +199,7 @@ void main()
   while ((dma[0x04 / 4] & 0x1000) != 0x1000)
     ;
 
-  int result[4][4];
+  int result[8][8];
 
   for (int num = 0; num < 3; num++)
   {
@@ -211,12 +210,12 @@ void main()
         ;
 
       printf("\n--- Sample %d Output ---\n", num - 1);
-      for (int j = 0; j < 4; j++)
+      for (int j = 0; j < 8; j++)
       {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 8; i++)
         {
-          printf("%6d ", dst[j * 4 + i]);
-          if (dst[j * 4 + i] != result[j][i])
+          printf("%6d ", dst[j * 8 + i]);
+          if (dst[j * 8 + i] != result[j][i])
           {
             printf("(Error Expecetd = %6d) ", result[j][i]);
           }
@@ -233,40 +232,40 @@ void main()
     // スループットを上げるにはこれ使った方がよさそうだし。]
     dma[0x30 / 4] = 1;
     dma[0x48 / 4] = dst_phys;
-    dma[0x58 / 4] = 4 * 4 * 4;
+    dma[0x58 / 4] = 8 * 8 * 4;
 
     if ((num + 1) != 3)
     {
-      for (int j = 0; j < 4; j++)
+      for (int j = 0; j < 8; j++)
       {
-        int sum[4] = {};
-        for (int k = 0; k < 8; k++)
+        int sum[8] = {};
+        for (int k = 0; k < 128; k++)
         {
-          for (int i = 0; i < 4; i++)
+          for (int i = 0; i < 8; i++)
           {
             sum[i] += matrix[i][k] * sample[j][k];
           }
         }
-        for (int oa = 0; oa < 4; oa++)
+        for (int oa = 0; oa < 8; oa++)
         {
           result[j][oa] = sum[oa];
         }
       }
       printf("\n--- Sample %d Input ---\n", num + 1);
-      for (int j = 0; j < 4; j++)
+      for (int j = 0; j < 8; j++)
       {
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 128; i++)
         {
           sample[j][i] = rand() & 0x000000ff;
           printf("%3d ", sample[j][i]);
-          src[j * 8 + i] = sample[j][i];
+          src[j * 128 + i] = sample[j][i];
         }
         printf("\n");
       }
       // AXI DMA transfer tx
       dma[0x00 / 4] = 1;
       dma[0x18 / 4] = src_phys;
-      dma[0x28 / 4] = 8 * 4 * 4;
+      dma[0x28 / 4] = 8 * 128 * 4;
 
       // Wait for the tx to finish
       while ((dma[0x04 / 4] & 0x1000) != 0x1000)
@@ -283,20 +282,20 @@ void main()
     ;
 
   printf("\n--- Sample %d Output ---\n", 2);
-  for (int j = 0; j < 4; j++)
+  for (int j = 0; j < 8; j++)
   {
-    int sum[4] = {};
+    int sum[8] = {};
     for (int k = 0; k < 8; k++)
     {
-      for (int i = 0; i < 4; i++)
+      for (int i = 0; i < 128; i++)
       {
         sum[i] += matrix[i][k] * sample[j][k];
       }
     }
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 8; i++)
     {
-      printf("%6d ", dst[j * 4 + i]);
-      if (dst[j * 4 + i] != sum[i])
+      printf("%6d ", dst[j * 8 + i]);
+      if (dst[j * 8 + i] != sum[i])
       {
         printf("(Error Expecetd = %6d) ", sum[i]);
       }
