fffe8c9c2d6a11b6f838e48ef0a7b26f4e157c76
Ohashi-Dev <ohashi.developer@gmail.com>
2022-09-18 21:53:30 +0900

    fix

--- src/buf/src_buf.sv
+++ src/buf/src_buf.sv
@@ -17,6 +17,23 @@ module src_buf
 
     // buff0とbuff1はp_ctrlで選択するやつ
 
+    always_ff @(posedge clk) begin
+                  if(exec & ~exec_src_addr[10] & ~exec_src_addr[0]) begin // 偶数
+                      exec_src_data <= buff0even[exec_src_addr[9:1]];
+                  end
+                  if(exec & ~exec_src_addr[10] & exec_src_addr[0]) begin // 奇数
+                      exec_src_data <= buff0odd[exec_src_addr[9:1]];
+                  end
+                  if(exec & exec_src_addr[10]) begin
+                      if (~exec_src_addr[0]) begin // 偶数
+                          exec_src_data <= buff1even[exec_src_addr[9:1]];
+                      end
+                      if (exec_src_addr[0]) begin // 奇数
+                          exec_src_data <= buff1odd[exec_src_addr[9:1]];
+                      end
+                  end
+              end;
+
     // buff0----------------------------------------------------------
     reg [31:0]        buff0even [0:511];
     reg [31:0]        buff0odd [0:511];
@@ -25,12 +42,6 @@ module src_buf
                   if(src_v & ~src_a[9]) begin
                       buff0even[src_a[8:0]] <= src_d[31:0];
                       buff0odd[src_a[8:0]] <= src_d[63:32];
-                  end
-                  if(exec & ~exec_src_addr[10] & ~exec_src_addr[0]) begin // 偶数
-                      exec_src_data <= buff0even[exec_src_addr[9:1]];
-                  end
-                  if(exec & ~exec_src_addr[10] & exec_src_addr[0]) begin // 奇数
-                      exec_src_data <= buff0odd[exec_src_addr[9:1]];
                   end
               end;
 
@@ -42,14 +53,6 @@ module src_buf
                   if(src_v & src_a[9]) begin
                       buff1even[src_a[8:0]] <= src_d[31:0];
                       buff1odd[src_a[8:0]] <= src_d[63:32];
-                  end
-                  if(exec & exec_src_addr[10]) begin
-                      if (~exec_src_addr[0]) begin // 偶数
-                          exec_src_data <= buff1even[exec_src_addr[9:1]];
-                      end
-                      if (exec_src_addr[0]) begin // 奇数
-                          exec_src_data <= buff1odd[exec_src_addr[9:1]];
-                      end
                   end
               end;
 
