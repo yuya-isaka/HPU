ba2d10e947e66e813693a15520eba35b90be9e28
Ohashi-Dev <ohashi.developer@gmail.com>
2022-10-05 15:05:42 +0900

    fix

---
+++ overlap_zynq_test/src/tb.cpp
@@ -0,0 +1,244 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vtop.h"
+#include <string.h>
+#include <iostream>
+
+// ====================================================================================================================================================================================
+
+union
+{
+  struct
+  {
+    unsigned int d0;
+    unsigned int d1;
+  };
+  uint64_t wd;
+} conv;
+
+vluint64_t main_time = 0;
+vluint64_t sim_start = 0;
+vluint64_t sim_end = sim_start + 300000;
+
+VerilatedVcdC *tfp;
+Vtop *verilator_top;
+
+// ====================================================================================================================================================================================
+
+void eval()
+{
+  // Neg edge
+  verilator_top->S_AXI_ACLK = 0;
+  verilator_top->AXIS_ACLK = 0;
+  // 立ち下がりの評価
+  verilator_top->eval();
+
+  if ((sim_start <= main_time) & ((main_time < sim_end) | (sim_end == 0)))
+  {
+    tfp->dump(main_time);
+  }
+  main_time += 5;
+
+  // --------------------------------------------------------------------
+
+  // Pos edge
+  verilator_top->S_AXI_ACLK = 1;
+  verilator_top->AXIS_ACLK = 1;
+  // 立ち上がりの評価
+  verilator_top->eval();
+
+  if ((main_time >= sim_start) & ((main_time < sim_end) | (sim_end == 0)))
+  {
+    tfp->dump(main_time);
+  }
+  main_time += 5;
+
+  return;
+}
+
+void printb(unsigned int v)
+{
+  unsigned int mask = (int)1 << (sizeof(v) * 8 - 1);
+  // printf("%lu\n", sizeof(v));
+  do
+    putchar(mask & v ? '1' : '0');
+  while (mask >>= 1);
+}
+
+void putb(unsigned int v)
+{
+  putchar('0'), putchar('b'), printb(v), putchar('\n');
+}
+
+// ====================================================================================================================================================================================
+
+int main(int argc, char **argv)
+{
+
+  ////////////////////////////////////////////////////////////////////////////// Verilator setup /////////////////////////////////////////////////////////////////////////////////////////
+
+  Verilated::commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+  tfp = new VerilatedVcdC;
+  verilator_top = new Vtop;
+  verilator_top->trace(tfp, 99); // requires explicit max levels param
+  tfp->open("tmp.vcd");
+  main_time = 0;
+
+  //////////////////////////////////////////////////////////////////////////////// initial begin ///////////////////////////////////////////////////////////////////////////////////////////
+
+  printf("\n ---------------------------- 開始 ----------------------------- \n");
+
+  verilator_top->S_AXI_BREADY = 1;
+  verilator_top->S_AXI_WSTRB = 15; // ストローブ信号,wstrb は wdata のうち実際に書き込む部位をバイト単位で指定します
+  verilator_top->S_AXI_RREADY = 1;
+  verilator_top->S_AXIS_TSTRB = 255; // ストリーム用のストローブ信号
+  verilator_top->S_AXIS_TLAST = 0;   // データ区切り信号
+  verilator_top->M_AXIS_TREADY = 1;
+
+  verilator_top->S_AXI_ARESETN = 0;
+  verilator_top->S_AXI_ACLK = 1;
+  verilator_top->AXIS_ARESETN = 0;
+  verilator_top->AXIS_ACLK = 1;
+  verilator_top->S_AXI_ARVALID = 0;
+  verilator_top->S_AXI_AWVALID = 0;
+  verilator_top->S_AXI_WVALID = 0;
+  verilator_top->S_AXIS_TVALID = 0;
+  verilator_top->eval();
+  main_time += 5;
+
+  eval();
+  eval();
+  verilator_top->S_AXI_ARESETN = 1;
+  verilator_top->AXIS_ARESETN = 1;
+  eval();
+  eval();
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // matw <- 1;
+  verilator_top->S_AXI_AWADDR = 0;
+  verilator_top->S_AXI_WDATA = 1;
+  verilator_top->S_AXI_AWVALID = 1;
+  verilator_top->S_AXI_WVALID = 1;
+  eval();
+  verilator_top->S_AXI_AWVALID = 0;
+  verilator_top->S_AXI_WVALID = 0;
+  eval();
+
+  verilator_top->S_AXI_ARVALID = 1;
+  eval();
+  eval();
+  while (0 != verilator_top->S_AXI_RDATA)
+  {
+    eval();
+  }
+  verilator_top->S_AXI_ARVALID = 0;
+  eval();
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // run <- 1;
+  verilator_top->S_AXI_AWADDR = 0;
+  verilator_top->S_AXI_WDATA = 2;
+  verilator_top->S_AXI_AWVALID = 1;
+  verilator_top->S_AXI_WVALID = 1;
+  eval();
+  verilator_top->S_AXI_AWVALID = 0;
+  verilator_top->S_AXI_WVALID = 0;
+  eval();
+
+  // 送信
+  verilator_top->S_AXIS_TVALID = 1;
+  for (int i = 0; i < 90; i += 2)
+  {
+    conv.d0 = i;
+    conv.d1 = i + 1;
+    verilator_top->S_AXIS_TDATA = conv.wd;
+    eval();
+  }
+  verilator_top->S_AXIS_TVALID = 0;
+  eval();
+
+  verilator_top->S_AXIS_TVALID = 1;
+  for (int i = 0; i < 90; i += 2)
+  {
+    conv.d0 = i;
+    conv.d1 = i + 1;
+    verilator_top->S_AXIS_TDATA = conv.wd;
+    eval();
+  }
+  verilator_top->S_AXIS_TVALID = 0;
+  eval();
+
+  // 演算終わって送られてくるの待つ
+  while (!verilator_top->M_AXIS_TVALID)
+  {
+    eval();
+  }
+
+  printf("\n ------------------------- Output -------------------------- \n\n");
+
+  for (int i = 0; i < 1; i++)
+  {
+    conv.wd = verilator_top->M_AXIS_TDATA;
+    printf("%u\n", conv.d0);
+    putb(conv.d0);
+    putb(conv.d1);
+    printf("\n");
+    eval();
+  }
+
+  // last <- 1;
+  verilator_top->S_AXI_AWADDR = 0;
+  verilator_top->S_AXI_WDATA = 6;
+  verilator_top->S_AXI_AWVALID = 1;
+  verilator_top->S_AXI_WVALID = 1;
+  eval();
+  verilator_top->S_AXI_AWVALID = 0;
+  verilator_top->S_AXI_WVALID = 0;
+  eval();
+
+  // 演算終わって送られてくるの待つ
+  while (!verilator_top->M_AXIS_TVALID)
+  {
+    eval();
+  }
+
+  printf("\n ------------------------- Output -------------------------- \n\n");
+
+  for (int i = 0; i < 1; i++)
+  {
+    conv.wd = verilator_top->M_AXIS_TDATA;
+    printf("%u\n", conv.d0);
+    putb(conv.d0);
+    putb(conv.d1);
+    printf("\n");
+    eval();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////// FPGA停止 run <- 0; last <- 0; //////////////////////////////////////////////////////////////////////
+
+  verilator_top->S_AXI_AWADDR = 0;
+  verilator_top->S_AXI_WDATA = 0;
+  verilator_top->S_AXI_AWVALID = 1;
+  verilator_top->S_AXI_WVALID = 1;
+  eval();
+  verilator_top->S_AXI_AWVALID = 0;
+  verilator_top->S_AXI_WVALID = 0;
+  eval();
+
+  eval();
+  eval();
+  verilator_top->S_AXI_ARESETN = 0;
+  verilator_top->AXIS_ARESETN = 0;
+  eval();
+  eval();
+
+  /////////////////////////////////////////////////////////////////////////////////////////  終了 ////////////////////////////////////////////////////////////////////////////////////
+
+  printf("\n ----------------------------- 終了 --------------------------------- \n");
+  delete verilator_top;
+  tfp->close();
+  return 0;
+}
