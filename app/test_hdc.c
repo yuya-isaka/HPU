3b90870cae15ccb524090b20b6fb2c9a78ce35d4
Ohashi-Dev <ohashi.developer@gmail.com>
2022-10-12 02:13:11 +0900

    fix test_hdc

--- app/test_hdc.c
+++ app/test_hdc.c
@@ -146,22 +146,22 @@ int main()
   if ((fd0 = open("/dev/udmabuf0", O_RDWR)) < 0)
   {
     perror("Failed: open /dev/udmabuf0");
-    return;
+    return 0;
   }
   if ((fd1 = open("/dev/udmabuf1", O_RDWR)) < 0)
   {
     perror("Failed: open /dev/udmabuf1");
-    return;
+    return 0;
   }
   if ((dmaf = open("/dev/uio0", O_RDWR | O_SYNC)) < 0)
   {
     perror("Falied: open /dev/uio0");
-    return;
+    return 0;
   }
   if ((topf = open("/dev/uio1", O_RDWR | O_SYNC)) < 0)
   {
     perror("Failed: open /dev/uio1");
-    return;
+    return 0;
   }
 
   // 対応表, 参考
@@ -174,18 +174,18 @@ int main()
   top = (int *)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, topf, 0);
   if (top == MAP_FAILED)
   {
-    perror("mmap");
+    perror("mmap top");
     close(topf);
-    return;
+    return 0;
   }
   // uio0をユーザ空間にマッピング
   // 4040_0000
   dma = (int *)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, dmaf, 0);
   if (dma == MAP_FAILED)
   {
-    perror("mmap");
+    perror("mmap dma");
     close(dmaf);
-    return;
+    return 0;
   }
 
   // udmabuf0をユーザ空間にマッピング
@@ -194,18 +194,18 @@ int main()
   src = (int *)mmap(NULL, 0x00080000, PROT_READ | PROT_WRITE, MAP_SHARED, fd0, 0);
   if (src == MAP_FAILED)
   {
-    perror("mmap");
+    perror("mmap src");
     close(fd0);
-    return;
+    return 0;
   }
   // udmabuf1をユーザ空間にマッピング
   // 0000_0000
   dst = (int *)mmap(NULL, 0x00080000, PROT_READ | PROT_WRITE, MAP_SHARED, fd1, 0);
   if (dst == MAP_FAILED)
   {
-    perror("mmap");
+    perror("mmap dst");
     close(fd1);
-    return;
+    return 0;
   }
 
   // dma reset
