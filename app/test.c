60afd97c7914840e495ea5f9ef41c14b5463487e
Ohashi-Dev <ohashi.developer@gmail.com>
2022-10-18 21:13:20 +0900

    fix

---
+++ app/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+int main()
+{
+	printf("\n ------------------------------ 開始 -------------------------------- \n");
+
+	return 0;
+}
