b8962dfb61673f97ccec9b0e7281f66a115754d0
yuya-isaka <zxwg6329@gmail.com>
2022-11-10 01:54:24 +0900

    fix 1024 test

--- src/tb.cpp
+++ src/tb.cpp
@@ -1319,11 +1319,11 @@ int main(int argc, char **argv)
   int DEBUG = 0;
   int ADDRNUM = 0;
   // 次元数可変 (結果を何個出力するかに使う)
-  const int DIM = 32 / 32;
-  // const int DIM = 1024 / 32;
+  // const int DIM = 32 / 32;
+  const int DIM = 1024 / 32;
   const int MAJORITY_ADDR = 1023;
 
-  const int SIMULATION_COUNT = 1000;
+  const int SIMULATION_COUNT = 100;
   for (int i = 3; i < SIMULATION_COUNT; i += 3)
   {
     ADDRNUM = i;
