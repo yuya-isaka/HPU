27c283d888ab8111dd3bfcd178ecaf24b89fbbc3
Ohashi-Dev <ohashi.developer@gmail.com>
2022-11-17 21:04:32 +0900

    perm check

--- app/benchmark/perm.c
+++ app/benchmark/perm.c
@@ -226,7 +226,7 @@ int main(int argc, char const *argv[])
 	start = clock();
 
 	const int trial_num = 50000000;
-	const int perm_num = 3;
+	const int perm_num = 1023;
 
 	int *addr = (int *)calloc(trial_num, sizeof(int));
 
