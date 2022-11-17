49bea05fbc6eda67410877b22ebafa2dfeb7b483
Ohashi-Dev <ohashi.developer@gmail.com>
2022-11-17 12:22:51 +0900

    fix

--- app/benchmark-ohashi/ohashi_bind_bound.c
+++ app/benchmark-ohashi/ohashi_bind_bound.c
@@ -103,8 +103,14 @@ uint16_t assemble(const char inst_str[], uint16_t addr)
 
 int main(int argc, char const *argv[])
 {
-	clock_t startstart = clock();
+	// プログラム全体の時間
+	clock_t start_program = clock();
+
+	// ----------------------------------------------------------------------------------------------------------------------------------------------
+
+	// 初期化の時間
 	clock_t start = clock();
+
 	puts("\n  -------------------------------------- HDC Program start ------------------------------------\n");
 
 	int fd0, fd1, dmaf, topf;
@@ -206,15 +212,16 @@ int main(int argc, char const *argv[])
 	while (dma[0x00 / 4] & 0x4)
 		;
 
-	top[0x00 / 4] = 2;
-
 	srand(10);
 
 	clock_t end = clock();
 	double time = ((double)(end - start)) / CLOCKS_PER_SEC * 1000.0;
-	printf("初期化コスト time %lf[ms]\n", time);
+	printf("初期化時間（CPU）: %lf[ms]\n", time);
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
+
+	// データ準備時間
+	start = clock();
 
 	const int core_num = 32;
 	const int trial_num = 50000000;
@@ -225,8 +232,8 @@ int main(int argc, char const *argv[])
 
 	int addr1[core_num];
 	int addr2[core_num];
-
-	start = clock();
+	int send_num_array[100];
+	int send_num_count = 0;
 
 	for (int i = 0; i < trial_num; i += core_num)
 	{
@@ -274,24 +281,7 @@ int main(int argc, char const *argv[])
 				uint16_t inst = assemble("last", 0);
 				src[send_num++] = inst;
 			}
-
-			dma[0x00 / 4] = 1;
-			dma[0x18 / 4] = src_phys;
-			dma[0x28 / 4] = send_num * 2; // 16ビットがsend_num個
-
-			dma[0x30 / 4] = 1;
-			dma[0x48 / 4] = dst_phys;
-			dma[0x58 / 4] = 128; // 32個 * 4バイト = 128バイト = 1024ビット
-
-			while ((dma[0x34 / 4] & 0x1000) != 0x1000)
-				;
-
-			dma[0x30 / 4] = 4;
-			dma[0x00 / 4] = 4;
-			while (dma[0x00 / 4] & 0x4)
-				;
-
-			send_num = 0;
+			send_num_array[send_num_count++] = send_num;
 		}
 	}
 
@@ -303,46 +293,59 @@ int main(int argc, char const *argv[])
 		uint16_t inst = assemble("last", 0);
 		src[send_num++] = inst;
 	}
+	send_num_array[send_num_count++] = send_num;
 
 	end = clock();
 	time = ((double)(end - start)) / CLOCKS_PER_SEC * 1000.0;
-	printf("毎回コスト time %lf[ms]\n", time);
+	printf("データ準備時間（CPU）: %lf[ms]\n", time);
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
-	// printf("%d\n", send_num);
-
+	// 計算時間（アクセラレータ）
 	start = clock();
 
-	dma[0x00 / 4] = 1;
-	dma[0x18 / 4] = src_phys;
-	// dma[0x28 / 4] = 25000001 * 2; // 16ビットがsend_num個
-	dma[0x28 / 4] = send_num * 2; // 16ビットがsend_num個
+	top[0x00 / 4] = 2;
 
-	dma[0x30 / 4] = 1;
-	dma[0x48 / 4] = dst_phys;
-	dma[0x58 / 4] = 128; // 32個 * 4バイト = 128バイト = 1024ビット
-
-	while ((dma[0x34 / 4] & 0x1000) != 0x1000)
-		;
-
-	for (int j = 0; j < 32; j++)
+	for (int i = 0; i < send_num_count; i++)
 	{
-		printf("%u\n", dst[j]);
+		int send_num_tmp = send_num_array[i];
+
+		dma[0x00 / 4] = 1;
+		dma[0x18 / 4] = src_phys;
+		dma[0x28 / 4] = send_num_tmp * 2; // 16ビットがsend_num個
+
+		dma[0x30 / 4] = 1;
+		dma[0x48 / 4] = dst_phys;
+		dma[0x58 / 4] = 128; // 32個 * 4バイト = 128バイト = 1024ビット
+
+		while ((dma[0x34 / 4] & 0x1000) != 0x1000)
+			;
+
+		dma[0x30 / 4] = 4;
+		dma[0x00 / 4] = 4;
+		while (dma[0x00 / 4] & 0x4)
+			;
+
+		src_phys += send_num_tmp * 2;
 	}
 
 	top[0x00 / 4] = 0;
 
 	end = clock();
 	time = ((double)(end - start)) / CLOCKS_PER_SEC * 1000.0;
-	printf("最後コスト time %lf[ms]\n", time);
+	printf("計算時間（アクセラレータ）: %lf[ms]\n", time);
+
+	for (int j = 0; j < 32; j++)
+	{
+		printf("%u\n", dst[j]);
+	}
 
 	// ----------------------------------------------------------------------------------------------------------------------------------------------
 
 	puts("\n  --------------------------------------- HDC Program end -------------------------------------\n");
 	end = clock();
-	time = ((double)(end - startstart)) / CLOCKS_PER_SEC * 1000.0;
-	printf("Ohashi_bind_bound time %lf[ms]\n", time);
+	time = ((double)(end - start_program)) / CLOCKS_PER_SEC * 1000.0;
+	printf("プログラム時間（CPU＋アクセラレータ）: %lf[ms]\n", time);
 
 	return 0;
 }
