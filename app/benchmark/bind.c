a22c68d9621ed448e60587e08ac23f2e5b146a4a
yuya-isaka <zxwg6329@gmail.com>
2022-11-18 11:27:47 +0900

    300万

--- app/benchmark/bind.c
+++ app/benchmark/bind.c
@@ -133,7 +133,8 @@ int main(int argc, char const *argv[])
 	start = clock();
 
 	// 試行回数
-	const int trial_num = 50000000;
+	// const int trial_num = 50000000;
+	const int trial_num = 3000000;
 
 	int *addr1 = (int *)calloc(trial_num, sizeof(int));
 	int *addr2 = (int *)calloc(trial_num, sizeof(int));
