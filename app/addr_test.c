79452b59ca68ec3288ebfea558a617c71b4b9d66
Ohashi-Dev <ohashi.developer@gmail.com>
2022-12-16 04:27:29 +0900

    fix

--- app/addr_test.c
+++ app/addr_test.c
@@ -7,525 +7,35 @@
 #include <sys/mman.h>
 #include <string.h>
 #include "hyper_vector.h"
-
-#define DIM 1024 / 32
-
-volatile int *top;
-volatile int *dma;
-volatile uint16_t *src;
-volatile int *dst;
-unsigned long src_phys;
-unsigned long dst_phys;
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// 渡された32ビットのvを、バイナリでコマンドライン出力
-void printb(unsigned int v)
-{
-  unsigned int mask = (int)1 << (sizeof(v) * 8 - 1);
-  do
-    putchar(mask & v ? '1' : '0');
-  while (mask >>= 1);
-}
-
-// printbを使いつつ整形して、バイナリをコマンドライン出力
-void putb(unsigned int v)
-{
-  putchar('0'), putchar('b'), printb(v), putchar('\n');
-}
-
-// 簡易アセンブラ
-// 10種類の命令
-uint16_t assemble(const char inst_str[], uint16_t addr)
-{
-  if (strcmp(inst_str, "load") == 0 || strcmp(inst_str, "wbitem") == 0 || strcmp(inst_str, "pwbitem") == 0 || strcmp(inst_str, "permute") == 0)
-  {
-    uint16_t result = 0;
-    uint16_t inst = 0;
-
-    // load
-    if (strcmp(inst_str, "load") == 0)
-    {
-      inst = 40960;
-      result = inst | addr;
-    }
-
-    // wb.item
-    else if (strcmp(inst_str, "wbitem") == 0)
-    {
-      inst = 36864;
-      result = inst | addr;
-    }
-
-    else if (strcmp(inst_str, "pwbitem") == 0)
-    {
-      inst = 53248;
-      result = inst | addr;
-    }
-
-    else if (strcmp(inst_str, "permute") == 0)
-    {
-      inst = 34816;
-      result = inst | addr;
-    }
-
-    return result;
-  }
-
-  else
-  {
-    uint16_t inst = 0;
-
-    // xor
-    if (strcmp(inst_str, "xor") == 0)
-    {
-      inst = 8192;
-    }
-
-    // pxor
-    if (strcmp(inst_str, "pxor") == 0)
-    {
-      inst = 24576;
-    }
-
-    // store
-    else if (strcmp(inst_str, "store") == 0)
-    {
-      inst = 4096;
-    }
-
-    // pstore
-    else if (strcmp(inst_str, "pstore") == 0)
-    {
-      inst = 20480;
-    }
-
-    // move
-    else if (strcmp(inst_str, "move") == 0)
-    {
-      inst = 2048;
-    }
-
-    // pmove
-    else if (strcmp(inst_str, "pmove") == 0)
-    {
-      inst = 18432;
-    }
-
-    // last
-    else if (strcmp(inst_str, "wb") == 0)
-    {
-      inst = 1024;
-    }
-
-    // wb
-    else if (strcmp(inst_str, "last") == 0)
-    {
-      inst = 512;
-    }
-
-    // nop
-    else if (strcmp(inst_str, "nop") == 0)
-    {
-      inst = 0;
-    }
-
-    else
-    {
-      printf("error");
-    }
-
-    return inst;
-  }
-}
-
-void hdc_init(void)
-{
-  int fd0, fd1, dmaf, topf;
-
-  if ((fd0 = open("/sys/class/u-dma-buf/udmabuf0/phys_addr", O_RDONLY)) != -1)
-  {
-    char attr[1024];
-    ssize_t done = read(fd0, attr, 1024);
-    if (done < 0)
-    {
-      perror("Failed: read /sys/class/u-dma-buf/udmabuf0/phys_addr");
-    }
-    sscanf(attr, "%lx", &src_phys);
-    close(fd0);
-  }
-
-  if ((fd0 = open("/sys/class/u-dma-buf/udmabuf1/phys_addr", O_RDONLY)) != -1)
-  {
-    char attr[1024];
-    ssize_t done = read(fd0, attr, 1024);
-    if (done < 0)
-    {
-      perror("Failed: read /sys/class/u-dma-buf/udmabuf1/phys_addr");
-    }
-    sscanf(attr, "%lx", &dst_phys);
-    close(fd0);
-  }
-
-  if ((fd0 = open("/dev/udmabuf0", O_RDWR)) < 0)
-  {
-    perror("Failed: open /dev/udmabuf0");
-    return;
-  }
-  if ((fd1 = open("/dev/udmabuf1", O_RDWR)) < 0)
-  {
-    perror("Failed: open /dev/udmabuf1");
-    return;
-  }
-  if ((dmaf = open("/dev/uio0", O_RDWR | O_SYNC)) < 0)
-  {
-    perror("Falied: open /dev/uio0");
-    return;
-  }
-  if ((topf = open("/dev/uio1", O_RDWR | O_SYNC)) < 0)
-  {
-    perror("Failed: open /dev/uio1");
-    return;
-  }
-
-  top = (int *)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, topf, 0);
-  if (top == MAP_FAILED)
-  {
-    perror("mmap top");
-    close(topf);
-    return;
-  }
-
-  dma = (int *)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, dmaf, 0);
-  if (dma == MAP_FAILED)
-  {
-    perror("mmap dma");
-    close(dmaf);
-    return;
-  }
-
-  src = (uint16_t *)mmap(NULL, 0x00080000, PROT_READ | PROT_WRITE, MAP_SHARED, fd0, 0);
-  if (src == MAP_FAILED)
-  {
-    perror("mmap src");
-    close(fd0);
-    return;
-  }
-
-  dst = (int *)mmap(NULL, 0x00080000, PROT_READ | PROT_WRITE, MAP_SHARED, fd1, 0);
-  if (dst == MAP_FAILED)
-  {
-    perror("mmap dst");
-    close(fd1);
-    return;
-  }
-}
-
-void make_imem(const int RANNUM)
-{
-  // item_memory_num (乱数の数)
-  top[0x04 / 4] = RANNUM - 1;
-
-  // gen <- 1;
-  top[0x00 / 4] = 1;
-
-  // 乱数生成終了を待つ
-  while (top[0x00 / 4] & 0x1)
-    ;
-}
-
-int send_num = 0;
-int BUSWIDTH = 0;
-int THREADSNUM = 0;
-
-void init(void)
-{
-  send_num = 0;
-  BUSWIDTH = 256;
-  THREADSNUM = 10;
-
-  // dma reset
-  dma[0x30 / 4] = 4;
-  dma[0x00 / 4] = 4;
-  while (dma[0x00 / 4] & 0x4)
-    ;
-}
-
-void nop_hv(void)
-{
-  src[send_num++] = 0;
-}
-
-void nop_hv_core(void)
-{
-  for (int i = 0; i < (BUSWIDTH / 16); i++)
-  {
-    nop_hv();
-  }
-}
-
-void load_hv(uint16_t addr)
-{
-  src[send_num++] = assemble("load", addr);
-}
-
-void load_hv_core(uint16_t core_num, uint16_t addr_array[core_num])
-{
-  for (int i = 0; i < core_num; i++)
-  {
-    load_hv(addr_array[i]);
-  }
-  for (int i = core_num; i < (BUSWIDTH / 16); i++)
-  {
-    nop_hv();
-  }
-}
-
-void load_hv_thread(uint16_t thread_num, uint16_t core_num, uint16_t addr_array[thread_num][core_num])
-{
-  for (int k = 0; k < thread_num; k++)
-  {
-    load_hv_core(core_num, addr_array[k]);
-  }
-  for (int k = thread_num; k < THREADSNUM; k++)
-  {
-    nop_hv_core();
-  }
-}
-
-void store_hv(void)
-{
-  src[send_num++] = assemble("store", 0);
-}
-
-void store_hv_core(uint16_t core_num)
-{
-  for (int i = 0; i < (BUSWIDTH / 16); i++)
-  {
-    if (i < core_num)
-    {
-      store_hv();
-    }
-    else
-    {
-      nop_hv();
-    }
-  }
-}
-
-void store_hv_thread(uint16_t thread_num, uint16_t core_num)
-{
-  for (int k = 0; k < THREADSNUM; k++)
-  {
-    if (k < thread_num)
-    {
-      store_hv_core(core_num);
-    }
-    else
-    {
-      nop_hv_core();
-    }
-  }
-}
-
-void move_hv(void)
-{
-  src[send_num++] = assemble("move", 0);
-}
-
-void move_hv_core(uint16_t core_num)
-{
-  for (int i = 0; i < (BUSWIDTH / 16); i++)
-  {
-    if (i < core_num)
-    {
-      move_hv();
-    }
-    else
-    {
-      nop_hv();
-    }
-  }
-}
-
-void move_hv_thread(uint16_t thread_num, uint16_t core_num)
-{
-  for (int k = 0; k < THREADSNUM; k++)
-  {
-    if (k < thread_num)
-    {
-      move_hv_core(core_num);
-    }
-    else
-    {
-      nop_hv_core();
-    }
-  }
-}
-
-void permute_hv(uint16_t permute_num)
-{
-  src[send_num++] = assemble("permute", permute_num);
-}
-
-void permute_hv_core(uint16_t core_num, uint16_t permute_num)
-{
-  for (int i = 0; i < (BUSWIDTH / 16); i++)
-  {
-    if (i < core_num)
-    {
-      permute_hv(permute_num);
-    }
-    else
-    {
-      nop_hv();
-    }
-  }
-}
-
-void permute_hv_thread(uint16_t thread_num, uint16_t core_num, uint16_t permute_num)
-{
-  for (int k = 0; k < THREADSNUM; k++)
-  {
-    if (k < thread_num)
-    {
-      permute_hv_core(core_num, permute_num);
-    }
-    else
-    {
-      nop_hv_core();
-    }
-  }
-}
-
-void pxor_hv(void)
-{
-  src[send_num++] = assemble("pxor", 0);
-}
-
-void pxor_hv_core(uint16_t core_num)
-{
-  for (int i = 0; i < (BUSWIDTH / 16); i++)
-  {
-    if (i < core_num)
-    {
-      pxor_hv();
-    }
-    else
-    {
-      nop_hv();
-    }
-  }
-}
-
-void pxor_hv_thread(uint16_t thread_num, uint16_t core_num)
-{
-  for (int k = 0; k < THREADSNUM; k++)
-  {
-    if (k < thread_num)
-    {
-      pxor_hv_core(core_num);
-    }
-    else
-    {
-      nop_hv_core();
-    }
-  }
-}
-
-void last_hv(void)
-{
-  src[send_num++] = assemble("last", 0);
-}
-
-void last_hv_core(uint16_t core_num)
-{
-  for (int i = 0; i < (BUSWIDTH / 16); i++)
-  {
-    if (i < core_num)
-    {
-      last_hv();
-    }
-    else
-    {
-      nop_hv();
-    }
-  }
-}
-
-void last_hv_thread(uint16_t thread_num, uint16_t core_num)
-{
-  for (int k = 0; k < THREADSNUM; k++)
-  {
-    if (k < thread_num)
-    {
-      last_hv_core(core_num);
-    }
-    else
-    {
-      nop_hv_core();
-    }
-  }
-}
-
-void hv_start(void)
-{
-  // run <- 1;
-  top[0x00 / 4] = 2;
-}
-
-void hv_send(void)
-{
-  // AXI DMA 送信の設定（UIO経由）
-  dma[0x00 / 4] = 1;
-  dma[0x18 / 4] = src_phys;
-  dma[0x28 / 4] = send_num * 2; // 16ビットがsend_num個
-
-  // 受信設定
-  // 送信チャネルの設定前に受信チャネルを設定すると変になるっぽい
-  dma[0x30 / 4] = 1;
-  dma[0x48 / 4] = dst_phys;
-  dma[0x58 / 4] = 128; // 32個 * 4バイト = 128バイト = 1024ビット
-
-  // 演算終了を待つ
-  while ((dma[0x34 / 4] & 0x1000) != 0x1000)
-    ;
-}
-
-void finish(void)
-{
-  // run <- 0;
-  top[0x00 / 4] = 0;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "hdc_processor.h"
 
 void check(const int NGRAM, const int CORENUM, const int ADDRNUM, const int MAJORITY_ADDR)
 {
-  init();
 
-  const int REMAINDAR = (ADDRNUM / NGRAM) % (CORENUM * THREADSNUM);
-  const int REMAINDAR_CORENUM = REMAINDAR / THREADSNUM;
+  hdc_init(0);
+
+  const int REMAINDAR = (ADDRNUM / NGRAM) % (CORENUM * THREADS_NUM);
+  const int REMAINDAR_CORENUM = REMAINDAR / THREADS_NUM;
   const int EVEN = ((ADDRNUM / NGRAM) % 2) == 0;
 
-  int LAST = (ADDRNUM / NGRAM) / (CORENUM * THREADSNUM);
+  int LAST = (ADDRNUM / NGRAM) / (CORENUM * THREADS_NUM);
   if (REMAINDAR == 0)
   {
     LAST--;
   }
-  LAST *= NGRAM * CORENUM * THREADSNUM;
+  LAST *= NGRAM * CORENUM * THREADS_NUM;
 
-  hv_start();
+  hdc_start();
 
   if (EVEN)
   {
     uint16_t addr_array[1][1] = {{MAJORITY_ADDR}};
-    load_hv_thread(1, 1, addr_array);
+    hdc_load_thread(1, 1, addr_array);
 
-    store_hv_thread(1, 1);
+    hdc_store_thread(1, 1);
   }
 
-  for (int j = 0; j < ADDRNUM; j += NGRAM * CORENUM * THREADSNUM)
+  for (int j = 0; j < ADDRNUM; j += NGRAM * CORENUM * THREADS_NUM)
   {
     uint16_t core_num;
     if (REMAINDAR != 0 && j == LAST)
@@ -537,9 +47,9 @@ void check(const int NGRAM, const int CORENUM, const int ADDRNUM, const int MAJO
       core_num = CORENUM;
     }
 
-    uint16_t addr_array[THREADSNUM][core_num];
+    uint16_t addr_array[THREADS_NUM][core_num];
 
-    for (int k = 0; k < THREADSNUM; k++)
+    for (int k = 0; k < THREADS_NUM; k++)
     {
       for (int i = 0; i < core_num; i++)
       {
@@ -548,14 +58,14 @@ void check(const int NGRAM, const int CORENUM, const int ADDRNUM, const int MAJO
     }
 
     // load ---------------------------------------------
-    load_hv_thread(THREADSNUM, core_num, addr_array);
+    hdc_load_thread(THREADS_NUM, core_num, addr_array);
     // ------------------------------------------------------
 
     // move ---------------------------------------------
-    move_hv_thread(THREADSNUM, core_num);
+    hdc_move_thread(THREADS_NUM, core_num);
     // ------------------------------------------------------
 
-    for (int k = 0; k < THREADSNUM; k++)
+    for (int k = 0; k < THREADS_NUM; k++)
     {
       for (int i = 0; i < core_num; i++)
       {
@@ -564,22 +74,22 @@ void check(const int NGRAM, const int CORENUM, const int ADDRNUM, const int MAJO
     }
 
     // load ---------------------------------------------
-    load_hv_thread(THREADSNUM, core_num, addr_array);
+    hdc_load_thread(THREADS_NUM, core_num, addr_array);
     // ------------------------------------------------------
 
     // permute ---------------------------------------------
-    permute_hv_thread(THREADSNUM, core_num, 1);
+    hdc_permute_thread(THREADS_NUM, core_num, 1);
     // ------------------------------------------------------
 
     // pxor ---------------------------------------------
-    pxor_hv_thread(THREADSNUM, core_num);
+    hdc_pxor_thread(THREADS_NUM, core_num);
     // ------------------------------------------------------
 
     // move ---------------------------------------------
-    move_hv_thread(THREADSNUM, core_num);
+    hdc_move_thread(THREADS_NUM, core_num);
     // ------------------------------------------------------
 
-    for (int k = 0; k < THREADSNUM; k++)
+    for (int k = 0; k < THREADS_NUM; k++)
     {
       for (int i = 0; i < core_num; i++)
       {
@@ -588,30 +98,30 @@ void check(const int NGRAM, const int CORENUM, const int ADDRNUM, const int MAJO
     }
 
     // load ---------------------------------------------
-    load_hv_thread(THREADSNUM, core_num, addr_array);
+    hdc_load_thread(THREADS_NUM, core_num, addr_array);
     // ------------------------------------------------------
 
     // permute ---------------------------------------------
-    permute_hv_thread(THREADSNUM, core_num, 2);
+    hdc_permute_thread(THREADS_NUM, core_num, 2);
     // ------------------------------------------------------
 
     // pxor ---------------------------------------------
-    pxor_hv_thread(THREADSNUM, core_num);
+    hdc_pxor_thread(THREADS_NUM, core_num);
     // ------------------------------------------------------
 
     // store ---------------------------------------------
-    store_hv_thread(THREADSNUM, core_num);
+    hdc_store_thread(THREADS_NUM, core_num);
     // ------------------------------------------------------
   }
 
   // LAST命令
   // 1024bit ---------------------------------------------
-  last_hv_core(1);
+  hdc_last_core(1);
   // ------------------------------------------------------
 
-  hv_send();
+  hdc_compute();
 
-  finish();
+  hdc_finish();
 
   // =========================================================================================================================================
 
@@ -645,9 +155,7 @@ void check(const int NGRAM, const int CORENUM, const int ADDRNUM, const int MAJO
 
   // hv_print(result);
 
-  // ===========================================================================================================================================
-
-  for (int i = 0; i < DIM; i++)
+  for (int i = 0; i < 32; i++)
   {
     if (result[i] != dst[i])
     {
@@ -670,9 +178,9 @@ int main()
 {
   printf("\n ------------------------------ 開始 -------------------------------- \n\n");
 
-  hdc_init();
+  hdc_setup();
 
-  make_imem(512);
+  hdc_make_imem(512);
 
   const int NGRAM = 3;
   const int CORENUM_MAX = 14;
@@ -683,9 +191,6 @@ int main()
     int ADDRNUM = i;
     check(NGRAM, CORENUM_MAX, ADDRNUM, MAJORITY_ADDR);
   }
-
-  // int ADDRNUM = 30;
-  // check(NGRAM, CORENUM_MAX, ADDRNUM, MAJORITY_ADDR);
 
   printf("\nOK!\n");
   printf("\n ------------------------------ 終了 -------------------------------- \n\n");
---
+++ app/hdc_processor.c
@@ -0,0 +1,678 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdint.h>
+#include "hdc_processor.h"
+
+// 簡易アセンブラ
+// 10種類の命令
+uint16_t hdc_assemble(const char inst_str[], uint16_t addr)
+{
+	if (strcmp(inst_str, "load") == 0 || strcmp(inst_str, "wbitem") == 0 || strcmp(inst_str, "pwbitem") == 0 || strcmp(inst_str, "permute") == 0)
+	{
+		uint16_t result = 0;
+		uint16_t inst = 0;
+
+		// load
+		if (strcmp(inst_str, "load") == 0)
+		{
+			inst = 40960;
+			result = inst | addr;
+		}
+
+		// wb.item
+		else if (strcmp(inst_str, "wbitem") == 0)
+		{
+			inst = 36864;
+			result = inst | addr;
+		}
+
+		else if (strcmp(inst_str, "pwbitem") == 0)
+		{
+			inst = 53248;
+			result = inst | addr;
+		}
+
+		else if (strcmp(inst_str, "permute") == 0)
+		{
+			inst = 34816;
+			result = inst | addr;
+		}
+
+		return result;
+	}
+
+	else
+	{
+		uint16_t inst = 0;
+
+		// xor
+		if (strcmp(inst_str, "xor") == 0)
+		{
+			inst = 8192;
+		}
+
+		// pxor
+		if (strcmp(inst_str, "pxor") == 0)
+		{
+			inst = 24576;
+		}
+
+		// store
+		else if (strcmp(inst_str, "store") == 0)
+		{
+			inst = 4096;
+		}
+
+		// pstore
+		else if (strcmp(inst_str, "pstore") == 0)
+		{
+			inst = 20480;
+		}
+
+		// move
+		else if (strcmp(inst_str, "move") == 0)
+		{
+			inst = 2048;
+		}
+
+		// pmove
+		else if (strcmp(inst_str, "pmove") == 0)
+		{
+			inst = 18432;
+		}
+
+		// wb
+		else if (strcmp(inst_str, "wb") == 0)
+		{
+			inst = 1024;
+		}
+
+		// last
+		else if (strcmp(inst_str, "last") == 0)
+		{
+			inst = 512;
+		}
+
+		// nop
+		else if (strcmp(inst_str, "nop") == 0)
+		{
+			inst = 0;
+		}
+
+		else
+		{
+			printf("error");
+		}
+
+		return inst;
+	}
+}
+
+void hdc_setup(void)
+{
+	int fd0, fd1, dmaf, topf;
+
+	if ((fd0 = open("/sys/class/u-dma-buf/udmabuf0/phys_addr", O_RDONLY)) != -1)
+	{
+		char attr[1024];
+		ssize_t done = read(fd0, attr, 1024);
+		if (done < 0)
+		{
+			perror("Failed: read /sys/class/u-dma-buf/udmabuf0/phys_addr");
+		}
+		sscanf(attr, "%lx", &src_phys);
+		close(fd0);
+	}
+
+	if ((fd0 = open("/sys/class/u-dma-buf/udmabuf1/phys_addr", O_RDONLY)) != -1)
+	{
+		char attr[1024];
+		ssize_t done = read(fd0, attr, 1024);
+		if (done < 0)
+		{
+			perror("Failed: read /sys/class/u-dma-buf/udmabuf1/phys_addr");
+		}
+		sscanf(attr, "%lx", &dst_phys);
+		close(fd0);
+	}
+
+	if ((fd0 = open("/dev/udmabuf0", O_RDWR)) < 0)
+	{
+		perror("Failed: open /dev/udmabuf0");
+		return;
+	}
+	if ((fd1 = open("/dev/udmabuf1", O_RDWR)) < 0)
+	{
+		perror("Failed: open /dev/udmabuf1");
+		return;
+	}
+	if ((dmaf = open("/dev/uio0", O_RDWR | O_SYNC)) < 0)
+	{
+		perror("Falied: open /dev/uio0");
+		return;
+	}
+	if ((topf = open("/dev/uio1", O_RDWR | O_SYNC)) < 0)
+	{
+		perror("Failed: open /dev/uio1");
+		return;
+	}
+
+	top = (int *)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, topf, 0);
+	if (top == MAP_FAILED)
+	{
+		perror("mmap top");
+		close(topf);
+		return;
+	}
+
+	dma = (int *)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, dmaf, 0);
+	if (dma == MAP_FAILED)
+	{
+		perror("mmap dma");
+		close(dmaf);
+		return;
+	}
+
+	src = (uint16_t *)mmap(NULL, 0x00080000, PROT_READ | PROT_WRITE, MAP_SHARED, fd0, 0);
+	if (src == MAP_FAILED)
+	{
+		perror("mmap src");
+		close(fd0);
+		return;
+	}
+
+	dst = (int *)mmap(NULL, 0x00080000, PROT_READ | PROT_WRITE, MAP_SHARED, fd1, 0);
+	if (dst == MAP_FAILED)
+	{
+		perror("mmap dst");
+		close(fd1);
+		return;
+	}
+}
+
+void hdc_make_imem(const int RANNUM)
+{
+	// item_memory_num (乱数の数)
+	top[0x04 / 4] = RANNUM - 1;
+
+	// gen <- 1;
+	top[0x00 / 4] = 1;
+
+	// 乱数生成終了を待つ
+	while (top[0x00 / 4] & 0x1)
+		;
+}
+
+void hdc_dma_reset(void)
+{
+	// dma reset
+	dma[0x30 / 4] = 4;
+	dma[0x00 / 4] = 4;
+	while (dma[0x00 / 4] & 0x4)
+		;
+}
+
+void hdc_init(const int N)
+{
+	SEND_NUM = N;
+}
+
+void hdc_start(void)
+{
+	// run <- 1;
+	top[0x00 / 4] = 2;
+}
+
+void hdc_compute(void)
+{
+	// AXI DMA 送信の設定（UIO経由）
+	dma[0x00 / 4] = 1;
+	dma[0x18 / 4] = src_phys;
+	dma[0x28 / 4] = SEND_NUM * 2; // 16ビットがsend_num個
+
+	// 受信設定
+	// 送信チャネルの設定前に受信チャネルを設定すると変になるっぽい
+	dma[0x30 / 4] = 1;
+	dma[0x48 / 4] = dst_phys;
+	dma[0x58 / 4] = 128; // 32個 * 4バイト = 128バイト = 1024ビット
+
+	// 演算終了を待つ
+	while ((dma[0x34 / 4] & 0x1000) != 0x1000)
+		;
+}
+
+void hdc_finish(void)
+{
+	// run <- 0;
+	top[0x00 / 4] = 0;
+}
+
+// =========================================================================================
+
+void hdc_nop(void)
+{
+	src[SEND_NUM++] = 0;
+}
+
+void hdc_nop_core(void)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		hdc_nop();
+	}
+}
+
+// =========================================================================================
+
+void hdc_load(uint16_t addr)
+{
+	src[SEND_NUM++] = hdc_assemble("load", addr);
+}
+
+void hdc_load_core(uint16_t core_num, uint16_t addr_array[core_num])
+{
+	for (int i = 0; i < core_num; i++)
+	{
+		hdc_load(addr_array[i]);
+	}
+	for (int i = core_num; i < (BUS_WIDTH / 16); i++)
+	{
+		hdc_nop();
+	}
+}
+
+void hdc_load_thread(uint16_t thread_num, uint16_t core_num, uint16_t addr_array[thread_num][core_num])
+{
+	for (int k = 0; k < thread_num; k++)
+	{
+		hdc_load_core(core_num, addr_array[k]);
+	}
+	for (int k = thread_num; k < THREADS_NUM; k++)
+	{
+		hdc_nop_core();
+	}
+}
+
+// =========================================================================================
+
+void hdc_wbitem(uint16_t addr)
+{
+	src[SEND_NUM++] = hdc_assemble("wbitem", addr);
+}
+
+void hdc_wbitem_core(uint16_t core_num, uint16_t addr_array[core_num])
+{
+	for (int i = 0; i < core_num; i++)
+	{
+		hdc_wbitem(addr_array[i]);
+	}
+	for (int i = core_num; i < (BUS_WIDTH / 16); i++)
+	{
+		hdc_nop();
+	}
+}
+
+void hdc_wbitem_thread(uint16_t thread_num, uint16_t core_num, uint16_t addr_array[thread_num][core_num])
+{
+	for (int k = 0; k < thread_num; k++)
+	{
+		hdc_wbitem_core(core_num, addr_array[k]);
+	}
+	for (int k = thread_num; k < THREADS_NUM; k++)
+	{
+		hdc_nop_core();
+	}
+}
+
+// =========================================================================================
+
+void hdc_pwbitem(uint16_t addr)
+{
+	src[SEND_NUM++] = hdc_assemble("pwbitem", addr);
+}
+
+void hdc_pwbitem_core(uint16_t core_num, uint16_t addr_array[core_num])
+{
+	for (int i = 0; i < core_num; i++)
+	{
+		hdc_pwbitem(addr_array[i]);
+	}
+	for (int i = core_num; i < (BUS_WIDTH / 16); i++)
+	{
+		hdc_nop();
+	}
+}
+
+void hdc_pwbitem_thread(uint16_t thread_num, uint16_t core_num, uint16_t addr_array[thread_num][core_num])
+{
+	for (int k = 0; k < thread_num; k++)
+	{
+		hdc_pwbitem_core(core_num, addr_array[k]);
+	}
+	for (int k = thread_num; k < THREADS_NUM; k++)
+	{
+		hdc_nop_core();
+	}
+}
+
+// =========================================================================================
+
+void hdc_store(void)
+{
+	src[SEND_NUM++] = hdc_assemble("store", 0);
+}
+
+void hdc_store_core(uint16_t core_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_store();
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
+
+void hdc_store_thread(uint16_t thread_num, uint16_t core_num)
+{
+	for (int k = 0; k < THREADS_NUM; k++)
+	{
+		if (k < thread_num)
+		{
+			hdc_store_core(core_num);
+		}
+		else
+		{
+			hdc_nop_core();
+		}
+	}
+}
+
+// =========================================================================================
+
+void hdc_pstore(void)
+{
+	src[SEND_NUM++] = hdc_assemble("pstore", 0);
+}
+
+void hdc_pstore_core(uint16_t core_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_pstore();
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
+
+void hdc_pstore_thread(uint16_t thread_num, uint16_t core_num)
+{
+	for (int k = 0; k < THREADS_NUM; k++)
+	{
+		if (k < thread_num)
+		{
+			hdc_pstore_core(core_num);
+		}
+		else
+		{
+			hdc_nop_core();
+		}
+	}
+}
+
+// =========================================================================================
+
+void hdc_move(void)
+{
+	src[SEND_NUM++] = hdc_assemble("move", 0);
+}
+
+void hdc_move_core(uint16_t core_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_move();
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
+
+void hdc_move_thread(uint16_t thread_num, uint16_t core_num)
+{
+	for (int k = 0; k < THREADS_NUM; k++)
+	{
+		if (k < thread_num)
+		{
+			hdc_move_core(core_num);
+		}
+		else
+		{
+			hdc_nop_core();
+		}
+	}
+}
+
+// =========================================================================================
+
+void hdc_pmove(void)
+{
+	src[SEND_NUM++] = hdc_assemble("pmove", 0);
+}
+
+void hdc_pmove_core(uint16_t core_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_pmove();
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
+
+void hdc_pmove_thread(uint16_t thread_num, uint16_t core_num)
+{
+	for (int k = 0; k < THREADS_NUM; k++)
+	{
+		if (k < thread_num)
+		{
+			hdc_pmove_core(core_num);
+		}
+		else
+		{
+			hdc_nop_core();
+		}
+	}
+}
+
+// =========================================================================================
+
+void hdc_permute(uint16_t permute_num)
+{
+	src[SEND_NUM++] = hdc_assemble("permute", permute_num);
+}
+
+void hdc_permute_core(uint16_t core_num, uint16_t permute_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_permute(permute_num);
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
+
+void hdc_permute_thread(uint16_t thread_num, uint16_t core_num, uint16_t permute_num)
+{
+	for (int k = 0; k < THREADS_NUM; k++)
+	{
+		if (k < thread_num)
+		{
+			hdc_permute_core(core_num, permute_num);
+		}
+		else
+		{
+			hdc_nop_core();
+		}
+	}
+}
+
+// =========================================================================================
+
+void hdc_xor(void)
+{
+	src[SEND_NUM++] = hdc_assemble("xor", 0);
+}
+
+void hdc_xor_core(uint16_t core_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_xor();
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
+
+void hdc_xor_thread(uint16_t thread_num, uint16_t core_num)
+{
+	for (int k = 0; k < THREADS_NUM; k++)
+	{
+		if (k < thread_num)
+		{
+			hdc_xor_core(core_num);
+		}
+		else
+		{
+			hdc_nop_core();
+		}
+	}
+}
+
+// =========================================================================================
+
+void hdc_pxor(void)
+{
+	src[SEND_NUM++] = hdc_assemble("pxor", 0);
+}
+
+void hdc_pxor_core(uint16_t core_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_pxor();
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
+
+void hdc_pxor_thread(uint16_t thread_num, uint16_t core_num)
+{
+	for (int k = 0; k < THREADS_NUM; k++)
+	{
+		if (k < thread_num)
+		{
+			hdc_pxor_core(core_num);
+		}
+		else
+		{
+			hdc_nop_core();
+		}
+	}
+}
+
+// =========================================================================================
+
+void hdc_wb(void)
+{
+	src[SEND_NUM++] = hdc_assemble("wb", 0);
+}
+
+void hdc_wb_core(uint16_t core_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_wb();
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
+
+void hdc_wb_thread(uint16_t thread_num, uint16_t core_num)
+{
+	for (int k = 0; k < THREADS_NUM; k++)
+	{
+		if (k < thread_num)
+		{
+			hdc_wb_core(core_num);
+		}
+		else
+		{
+			hdc_nop_core();
+		}
+	}
+}
+
+// =========================================================================================
+
+void hdc_last(void)
+{
+	src[SEND_NUM++] = hdc_assemble("last", 0);
+}
+
+void hdc_last_core(uint16_t core_num)
+{
+	for (int i = 0; i < (BUS_WIDTH / 16); i++)
+	{
+		if (i < core_num)
+		{
+			hdc_last();
+		}
+		else
+		{
+			hdc_nop();
+		}
+	}
+}
