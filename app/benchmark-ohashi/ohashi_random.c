a15070bccc7b1287f41c5c54a1d06f7e6d29beb0
Ohashi-Dev <ohashi.developer@gmail.com>
2023-01-06 09:37:06 +0900

    fix

--- app/benchmark-ohashi/ohashi_random.c
+++ app/benchmark-ohashi/ohashi_random.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>	  // open
 #include <unistd.h>	  // read
 #include <sys/mman.h> // mmap
+#include <time.h>
 #include "hdc_processor.h"
 
 int main(int argc, char const *argv[])
@@ -15,10 +16,15 @@ int main(int argc, char const *argv[])
 	// メモリセットアップ
 	hdc_setup();
 
+	clock_t START_COMPUTE = clock();
 	// アイテムメモリ生成
 	hdc_make_imem(RANNUM);
+	clock_t END_COMPUTE = clock();
+	double COM_TIME = ((double)(END_COMPUTE - START_COMPUTE)) / CLOCKS_PER_SEC;
 
 	hdc_finish();
+
+	printf("\n  計算時間: %lf[s]\n", COM_TIME);
 
 	return 0;
 }
