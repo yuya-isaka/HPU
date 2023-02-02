b1db27ebf4e6af4ae3568fcf2875d2411f761e9e
yuya-isaka <zxwg6329@gmail.com>
2023-02-03 01:15:09 +0900

    fix benchmark

--- pynq_src/app/benchmark-ohashi/ohashi_random.c
+++ pynq_src/app/benchmark-ohashi/ohashi_random.c
@@ -9,6 +9,33 @@
 #include <time.h>
 #include "hdc_processor.h"
 
+uint32_t xor128(int reset)
+{
+	// 初期値
+	static uint32_t x = 123456789;
+	static uint32_t y = 362436069;
+	static uint32_t z = 521288629;
+	static uint32_t w = 88675123;
+
+	// リセット信号
+	if (reset)
+	{
+		x = 123456789;
+		y = 362436069;
+		z = 521288629;
+		w = 88675123;
+		return 0;
+	}
+	else
+	{
+		uint32_t t = x ^ (x << 11);
+		x = y;
+		y = z;
+		z = w;
+		return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	const int RANNUM = 512;
